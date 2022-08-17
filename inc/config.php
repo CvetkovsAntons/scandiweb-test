d6eac31a2a171a86952f423487c061e0b91a141a
Antons Cvetkovs <antonscvetkovs12@gmail.com>
2022-08-17 13:27:09 +0300

    config changeds

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,12 @@
 <?php
 
-const DB_HOST = "localhost";
-const DB_USERNAME = "user";
-const DB_PASSWORD = "user";
-const DB_DATABASE = "scandiweb_test";
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+define("DB_HOST", $cleardb_url["host"]);
+define("DB_USERNAME", $cleardb_url["user"]);
+define("DB_PASSWORD", $cleardb_url["pass"]);
+define("DB_DATABASE", substr($cleardb_url["path"], 1));
+
+$active_group = 'default';
+$query_builder = TRUE;
